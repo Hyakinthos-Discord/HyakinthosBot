87c8f3b6917e7d9ff0447663e6d5499170393040
TheCuddlyBear <basvthazeveld@outlook.com>
2023-12-05 19:43:12 +0100

    Improve handling of music extensions in Kotlin Bot script
    Significant changes made to the MusicExtension.kt in regards to the instances of playing and loading music tracks. Additional checks have added for player and queues, providing detailed information on the current track and queuing process. The modifications were done to circumvent potential issues within the music player, such as null track exception, omitted error messages, and enhance overall bot-functioning concerning music streaming. Changes have led to code elaboration, making it more intuitive and easier to understand.

--- src/main/kotlin/me/thecuddlybear/Bot/extensions/MusicExtension.kt
+++ src/main/kotlin/me/thecuddlybear/Bot/extensions/MusicExtension.kt
@@ -41,14 +41,34 @@ class MusicExtension : Extension() {
                 val channelId = voiceState?.channelId.toString()
 
                 val link = lavalink.getLink(guild?.id.toString())
+                val player = link.player
+
+                if (!queues.containsKey(guild?.id.toString())){
+                    listeners[guild?.id.toString()] = channel.id.toString()
+                    queues[guild?.id.toString()] = ArrayDeque(0)
+
+                    player.on<TrackStartEvent> {
+                        val channelID = listeners[guild?.id.toString()]
+
+                        val channel = bot.kordRef.getGuild(Snowflake(guildId)).getChannel(Snowflake(channelID.toString())).asChannelOf<TextChannel>()
+                        channel.createMessage {
+                            content = "Now playing: `${track.info.title} - ${track.info.author}`"
+                        }
+
+                    }
+
+                    player.on<TrackEndEvent> {
+                        if(queues[guildId.toString()]?.isNotEmpty() == true){
+                            player.playTrack(track = queues[guildId.toString()]?.removeFirst() as Track)
+                        }
+                    }
+                }
 
                 if(channelId == null){
                     respond{
                         content = "Please connect to a voice channel!"
                     }
                 }
-
-                val player = link.player
 
                 listeners[guild?.id.toString()] = channel.id.toString()
 
@@ -57,44 +77,44 @@ class MusicExtension : Extension() {
                 }
 
                 if(link.state != Link.State.CONNECTED){
-                    respond { content = "I am not connected to a VC right now, try /connect" }
+                    link.connect(channelId)
+                }
+
+                val query = arguments.url
+                val search = if (query.startsWith("http")) {
+                    query
                 }else{
-                    val query = arguments.url
-                    val search = if (query.startsWith("http")) {
-                        query
-                    }else{
-                        "ytsearch:$query"
+                    "ytsearch:$query"
+                }
+
+                when (val item = link.loadItem(search)) {
+                    is LoadResult.TrackLoaded -> {
+                        if(player.playingTrack == null){
+                            player.playTrack(track = item.data)
+                            respond { content = "Added `${item.data.info.title} - ${item.data.info.author}` to the queue!" }
+                        }else{
+                            queues[guild?.id.toString()]?.add(item.data)
+                            respond { content = "Added `${item.data.info.title} - ${item.data.info.author}` to the queue!" }
+                        }
+                    }
+                    is LoadResult.PlaylistLoaded -> {
+                        queues[guild?.id.toString()]?.addAll(item.data.tracks)
+
+                        if(player.playingTrack == null){
+                            player.playTrack(track = queues[guild?.id.toString()]?.removeFirst() as Track)
+                        }
+                        respond { content = "Added the playlist to the queue!" }
                     }
 
-                    when (val item = link.loadItem(search)) {
-                        is LoadResult.TrackLoaded -> {
-                            if(player.playingTrack == null){
-                                player.playTrack(track = item.data)
-                                respond { content = "Playing the track" }
-                            }else{
-                                queues[guild?.id.toString()]?.add(item.data)
-                                respond { content = "Added ```${item.data.info.title} - ${item.data.info.author}``` to the queue!" }
-                            }
-                        }
-                        is LoadResult.PlaylistLoaded -> {
-                            queues[guild?.id.toString()]?.addAll(item.data.tracks)
-
-                            if(player.playingTrack == null){
-                                player.playTrack(track = queues[guild?.id.toString()]?.removeFirst() as Track)
-                            }
-                            respond { content = "Added the playlist to the queue!" }
-                        }
-
-                        is LoadResult.LoadFailed -> respond { content = item.data.message ?: "Exception" }
-                        is LoadResult.NoMatches -> respond { content = "Could not find the song"}
-                        is LoadResult.SearchResult -> {
-                            if(player.playingTrack == null){
-                                player.playTrack(track = item.data.tracks.first())
-                                respond { content = "Playing the track" }
-                            }else{
-                                queues[guild?.id.toString()]?.add(item.data.tracks.first())
-                                respond { content = "Added ```${item.data.tracks.first().info.title} - ${item.data.tracks.first().info.author}``` to the queue!" }
-                            }
+                    is LoadResult.LoadFailed -> respond { content = item.data.message ?: "Exception" }
+                    is LoadResult.NoMatches -> respond { content = "Could not find the song"}
+                    is LoadResult.SearchResult -> {
+                        if(player.playingTrack == null){
+                            player.playTrack(track = item.data.tracks.first())
+                            respond { content = "Playing the track" }
+                        }else{
+                            queues[guild?.id.toString()]?.add(item.data.tracks.first())
+                            respond { content = "Added `${item.data.tracks.first().info.title} - ${item.data.tracks.first().info.author}` to the queue!" }
                         }
                     }
                 }
@@ -112,22 +132,24 @@ class MusicExtension : Extension() {
                 val link = lavalink.getLink(guild?.id.toString())
                 val player = link.player
 
-                listeners[guild?.id.toString()] = channel.id.toString()
-                queues[guild?.id.toString()] = ArrayDeque(0)
+                if (!queues.containsKey(guild?.id.toString())){
+                    listeners[guild?.id.toString()] = channel.id.toString()
+                    queues[guild?.id.toString()] = ArrayDeque(0)
 
-                player.on<TrackStartEvent> {
-                    val channelID = listeners[guild?.id.toString()]
+                    player.on<TrackStartEvent> {
+                        val channelID = listeners[guild?.id.toString()]
 
-                    val channel = bot.kordRef.getGuild(Snowflake(guildId)).getChannel(Snowflake(channelID.toString())).asChannelOf<TextChannel>()
-                    channel.createMessage {
-                        content = "Now playing: ```${track.info.title} - ${track.info.author}```"
+                        val channel = bot.kordRef.getGuild(Snowflake(guildId)).getChannel(Snowflake(channelID.toString())).asChannelOf<TextChannel>()
+                        channel.createMessage {
+                            content = "Now playing: ```${track.info.title} - ${track.info.author}```"
+                        }
+
                     }
 
-                }
-
-                player.on<TrackEndEvent> {
-                    if(queues[guildId.toString()]?.isNotEmpty() == true){
-                        player.playTrack(track = queues[guildId.toString()]?.removeFirst() as Track)
+                    player.on<TrackEndEvent> {
+                        if(queues[guildId.toString()]?.isNotEmpty() == true){
+                            player.playTrack(track = queues[guildId.toString()]?.removeFirst() as Track)
+                        }
                     }
                 }
 
@@ -219,7 +241,6 @@ class MusicExtension : Extension() {
                 queues[guild?.id.toString()] = ArrayDeque<Track>(0)
                 player.stopTrack()
                 link.disconnectAudio()
-                link.destroy()
 
                 respond {
                     content = "Stopped playing!"
